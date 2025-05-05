9dabbb8b06c5c460e3e3aca1efbf24b689e390a1
MinhajSiam <minhajsiam007@gmail.com>
2025-05-06 00:57:36 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,21 @@
+const myAge = 27;
+console.log(1, "My age is:", myAge);
+
+const siblings = 3;
+console.log(2, "We are ", siblings, "brother and sisters.");
+
+const students = 10;
+console.log();
+
+const marks = 84;
+console.log();
+
+const rooms = 5;
+console.log();
+
+const studyTime = 3;
+console.log();
+
+const mobileDigit = 0 + 1 + 8 + 2 + 9 + 6 + 7 + 5 + 4 + 5 + 2;
+const total = mobileDigit;
+console.log();
