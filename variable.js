169c569c847e2d60f6c65559fe114cbe9cdbbcb9
MinhajSiam <minhajsiam007@gmail.com>
2025-05-06 01:00:01 +0600

    update

--- variable.js
+++ variable.js
@@ -5,17 +5,17 @@ const siblings = 3;
 console.log(2, "We are ", siblings, "brother and sisters.");
 
 const students = 10;
-console.log();
+console.log(students);
 
 const marks = 84;
-console.log();
+console.log(marks);
 
 const rooms = 5;
-console.log();
+console.log(rooms);
 
 const studyTime = 3;
-console.log();
+console.log(studyTime);
 
 const mobileDigit = 0 + 1 + 8 + 2 + 9 + 6 + 7 + 5 + 4 + 5 + 2;
 const total = mobileDigit;
-console.log();
+console.log(total);
